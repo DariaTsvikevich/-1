3d6e07b2a4eb1c8c1009f5257935dd3b77041426
Daria <tsvikevich.daria@gmail.com>
2022-10-13 12:00:05 +0300

    Work seminar added

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача 1.
+
+/*
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2 * n2;
+
+if(n1 == quad2)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
+*/
+
+//Задача 2. 
+/*
+Console.Write("Input a positive int number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+//Задача 3.
+
+/*
+31625 % 10 = 5
+31625 % 100 = 25
+31625 % 1000 = 625
+
+31625 / 10 = 3162
+31625 / 100 = 316
+31625 / 1000 = 31
+*/
+
+/*
+Console.Write("Input a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+Console.WriteLine($"Last digit of {num} is {result}");
+*/
+
+
